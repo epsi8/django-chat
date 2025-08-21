9ef6ff0b08963faf4632911ce25fcf2f259ec9bc
epsi8 <epsithatekkoli@gmaile.com>
2025-08-21 15:18:21 +0530

    Initial commit

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -108,3 +108,11 @@ DATABASES = {
     )
 }
 
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.getenv("REDIS_URL")],
+        },
+    },
+}
