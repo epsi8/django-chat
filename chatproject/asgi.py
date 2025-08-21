ea1332cda625e9536ccdd032f9a2d723c1823a94
epsi8 <epsithatekkoli@gmaile.com>
2025-08-21 14:52:49 +0530

    Fix: set DJANGO_SETTINGS_MODULE in asgi.py

--- chatproject/asgi.py
+++ chatproject/asgi.py
@@ -1,14 +1,19 @@
 import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-import chat.routing
+from channels.auth import AuthMiddlewareStack
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatproject.settings")  # your project
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatproject.settings")
 django.setup()
+
+import chat.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
